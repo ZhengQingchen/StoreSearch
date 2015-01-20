b637d9b051acd18e65124ee0cfe02120c55ee8fa
mac <mac@macdeMacBook-Pro.local>
2015-01-20 11:51:16 +0800

    SearchBar function added

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by mac on 15/1/20.
+//  Copyright (c) 2015年 mac. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
